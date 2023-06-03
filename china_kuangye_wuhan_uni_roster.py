144d7dc21b4e1dac8a8194ff4ec3b70dd692542b
yjsdl <yj160521@163.com>
2023-06-03 18:29:31 +0800

    删除

---
+++ china_kuangye_wuhan_uni_roster.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+# @date：2023/5/25 17:14
+# @Author：LiuYiJie
+# @file： xibei_nongling_keji_uni_roster
+from school_code.aa_commen import TeacherEmail
+
+collage_lists = [
+{
+            'name': '矿业工程学院',
+            'school_urls': [
+                {'link': 'https://cese.cumt.edu.cn/szdw/xyjs.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cese.cumt.edu.cn/szdw/dsdw/sssds.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cese.cumt.edu.cn/szdw/dsdw/bssds.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cese.cumt.edu.cn/szdw/jcrc.htm', 'docu': 'con_wrap fr'},
+            ]
+        },
+{
+            'name': '安全工程学院',
+            'school_urls': [
+                {'link': 'https://safe.cumt.edu.cn/szdw/szjs.htm', 'docu': 'listing'},
+                {'link': 'https://safe.cumt.edu.cn/szdw/jcrc.htm', 'docu': 'listing'},
+            ]
+        },
+{
+            'name': '力学与土木工程学院',
+            'school_urls': [
+                {'link': 'https://cace.cumt.edu.cn/szdw/jsdw.htm', 'docu': 'n_join'},
+                {'link': 'https://cace.cumt.edu.cn/szdw/jcrc.htm', 'docu': 'n_talent'},
+            ]
+        },
+{
+            'name': '机电工程学院',
+            'school_urls': [
+                {'link': 'https://cmee.cumt.edu.cn/szdw/jsml/jxsjx.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cmee.cumt.edu.cn/szdw/jsml/jxdzx.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cmee.cumt.edu.cn/szdw/jsml/jxzzx.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cmee.cumt.edu.cn/szdw/jsml/jqrgcx.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cmee.cumt.edu.cn/szdw/jsml/gctxzx.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cmee.cumt.edu.cn/szdw/jsml/syzx.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cmee.cumt.edu.cn/szdw/dsdw/sssds.htm', 'docu': 'con_wrap fr'},
+                {'link': 'https://cmee.cumt.edu.cn/szdw/dsdw/bssds.htm', 'docu': 'con_wrap fr'},
+            ]
+        },
+{
+            'name': '信息与控制工程学院',
+            'school_urls': [
+                {'link': 'https://siee.cumt.edu.cn/szdw/zrjs.htm', 'docu': 'pull-right right-box'},
+                {'link': 'https://siee.cumt.edu.cn/szdw/syzxjs.htm', 'docu': 'pull-right right-box'},
+                {'link': 'https://siee.cumt.edu.cn/szdw/yjgjs.htm', 'docu': 'pull-right right-box'},
+                {'link': 'https://siee.cumt.edu.cn/szdw/rxjzgmd.htm', 'docu': 'pull-right right-box'},
+            ]
+        },
+{
+            'name': '资源与地球科学学院',
+            'school_urls': [
+                {'link': 'https://sres.cumt.edu.cn/szdw1/jsdw1/dzx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/jsdw1/dzgcx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/jsdw1/dqwlx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/jsdw1/swyszyx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/jsdw1/dqxxkxx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/bssds/dzx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/bssds/dzgcx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/bssds/dqwlx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/bssds/swyszyx.htm', 'docu': 'pull-right list-right'},
+                {'link': 'https://sres.cumt.edu.cn/szdw1/bssds/dqxxkxx.htm', 'docu': 'pull-right list-right'},
+            ]
+        },
+{
+            'name': '化工学院',
+            'school_urls': [
+                {'link': 'https://scet.cumt.edu.cn/20574/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/20575/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/20576/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/20577/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/20578/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/nhx/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/kwjggc_21915/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/hxgcygy_21916/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/yyhx/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/sgx_21918/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/gkx_21919/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/20572/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://scet.cumt.edu.cn/ysfc/list.htm', 'docu': 'col_news_con'},
+            ]
+        },
+{
+            'name': '环境与测绘学院',
+            'school_urls': [
+                {'link': 'https://cesi.cumt.edu.cn/szdw/jgml1/js.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/jgml1/fjs.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/jgml1/js1.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/jgml1/bsh.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/jstd.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/bssds1/xnbd.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/bssds1/xwbd.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/sssds/sssds1.htm', 'docu': 'right-con'},
+                {'link': 'https://cesi.cumt.edu.cn/szdw/jcrc.htm', 'docu': 'right-con'},
+            ]
+        },
+    {
+        'name': '电气工程学院',
+        'school_urls': [
+            {'link': 'https://see.cumt.edu.cn/szdw/szdw.htm', 'docu': 'con_wrap fr'},
+        ]
+    },
+{
+            'name': '低碳能源与动力工程学院',
+            'school_urls': [
+                {'link': 'https://slepe.cumt.edu.cn/szdw/szdw.htm', 'docu': 'listing'},
+            ]
+        },
+{
+            'name': '材料与物理学院',
+            'school_urls': [
+                {'link': 'https://smsp.cumt.edu.cn/21999/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/22000/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/22001/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/22002/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/22003/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/22004/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/22005/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/22006/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/21998/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://smsp.cumt.edu.cn/21996/list.htm', 'docu': 'col_news_con'},
+            ]
+        },
+{
+            'name': '数学学院',
+            'school_urls': [
+                {'link': 'https://math.cumt.edu.cn/5649/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/fjsfc/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/jsfc/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/bshfc/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/9121/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/9122/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/9123/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/9124/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/9125/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://math.cumt.edu.cn/9126/list.htm', 'docu': 'col_news_con'},
+            ]
+        },
+{
+            'name': '计算机科学与技术学院',
+            'school_urls': [
+                {'link': 'https://cs.cumt.edu.cn/szdw/xyjs.htm', 'docu': 'tab_wrap tab_wrap1 aos-init aos-animate'},
+            ]
+        },
+{
+            'name': '经济管理学院',
+            'school_urls': [
+                {'link': 'https://sm.cumt.edu.cn/5056/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://sm.cumt.edu.cn/64/d5/c5057a615637/page.htm', 'docu': 'wp_articlecontent'},
+                {'link': 'https://sm.cumt.edu.cn/65/6f/c5058a615791/page.htm', 'docu': 'wp_articlecontent'},
+                {'link': 'https://sm.cumt.edu.cn/5059/list.htm', 'docu': 'col_news_con'},
+            ]
+        },
+{
+            'name': '公共管理学院（应急管理学院）',
+            'school_urls': [
+                {'link': 'https://cllp.cumt.edu.cn/szdw1/xzglx.htm', 'docu': 'right-con'},
+                {'link': 'https://cllp.cumt.edu.cn/szdw1/tdzyglx.htm', 'docu': 'right-con'},
+                {'link': 'https://cllp.cumt.edu.cn/szdw1/yjglx.htm', 'docu': 'right-con'},
+                {'link': 'https://cllp.cumt.edu.cn/szdw1/jyjjyglyjs.htm', 'docu': 'right-con'},
+                {'link': 'https://cllp.cumt.edu.cn/szdw1/syzx.htm', 'docu': 'right-con'},
+                {'link': 'https://cllp.cumt.edu.cn/szdw1/yjg1.htm', 'docu': 'right-con'},
+                {'link': 'https://cllp.cumt.edu.cn/szdw1/bssdsdw.htm', 'docu': 'right-con'},
+            ]
+        },
+{
+            'name': '马克思主义学院',
+            'school_urls': [
+                {'link': 'https://mks.cumt.edu.cn/szdw/jsml/jsjj.htm', 'docu': 'pull-right right-box'},
+                {'link': 'https://mks.cumt.edu.cn/szdw/jsml/fjsjj.htm', 'docu': 'pull-right right-box'},
+                {'link': 'https://mks.cumt.edu.cn/szdw/jsml/jsjian.htm', 'docu': 'pull-right right-box'},
+                {'link': 'https://mks.cumt.edu.cn/szdw/jsml/dsmd.htm', 'docu': 'pull-right right-box'},
+            ]
+        },
+{
+            'name': '外国语言文化学院',
+            'school_urls': [
+                {'link': 'https://sfs.cumt.edu.cn/xbs/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://sfs.cumt.edu.cn/651/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://sfs.cumt.edu.cn/649/list.htm', 'docu': 'col_news_con'},
+            ]
+        },
+{
+            'name': '建筑与设计学院',
+            'school_urls': [
+                {'link': 'https://art.cumt.edu.cn/3198/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://art.cumt.edu.cn/3199/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://art.cumt.edu.cn/3201/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://art.cumt.edu.cn/qnjs/list.htm', 'docu': 'col_news_con'},
+                {'link': 'https://art.cumt.edu.cn/dzgl/list.htm', 'docu': 'col_news_con'},
+            ]
+        },
+{
+            'name': '人文与艺术学院',
+            'school_urls': [
+                {'link': 'https://rwxy.cumt.edu.cn/szdw1/zgyywx.htm', 'docu': 'right-con'},
+                {'link': 'https://rwxy.cumt.edu.cn/szdw1/fx.htm', 'docu': 'right-con'},
+                {'link': 'https://rwxy.cumt.edu.cn/szdw1/xwcbx.htm', 'docu': 'right-con'},
+                {'link': 'https://rwxy.cumt.edu.cn/szdw1/ylx.htm', 'docu': 'right-con'},
+                {'link': 'https://rwxy.cumt.edu.cn/szdw1/msx.htm', 'docu': 'right-con'},
+                {'link': 'https://rwxy.cumt.edu.cn/szdw1/rxjs.htm', 'docu': 'right-con'},
+            ]
+        },
+{
+            'name': '体育学院',
+            'school_urls': [
+                {'link': 'https://sport.cumt.edu.cn/b2/51/c21911a635473/page.htm', 'docu': 'wp_articlecontent'},
+                {'link': 'https://sport.cumt.edu.cn/be/03/c10320a638467/page.htm', 'docu': 'wp_articlecontent'},
+                {'link': 'https://sport.cumt.edu.cn/be/00/c10320a638464/page.htm', 'docu': 'wp_articlecontent'},
+                {'link': 'https://sport.cumt.edu.cn/bd/f9/c10320a638457/page.htm', 'docu': 'wp_articlecontent'},
+            ]
+        },
+{
+            'name': '孙越崎学院',
+            'school_urls': [
+                {'link': 'https://syq.cumt.edu.cn/98/8a/c12563a563338/page.htm', 'docu': 'wp_articlecontent'},
+                {'link': 'https://syq.cumt.edu.cn/98/ac/c12563a563372/page.htm', 'docu': 'wp_articlecontent'},
+                {'link': 'https://syq.cumt.edu.cn/12564/list.htm', 'docu': 'column-news-con'},
+                {'link': 'https://syq.cumt.edu.cn/12565/list.htm', 'docu': 'column-news-con'},
+                {'link': 'https://syq.cumt.edu.cn/lxl/list.htm', 'docu': 'column-news-con'},
+                {'link': 'https://syq.cumt.edu.cn/12567/list.htm', 'docu': 'column-news-con'},
+                {'link': 'https://syq.cumt.edu.cn/12568/list.htm', 'docu': 'column-news-con'},
+                {'link': 'https://syq.cumt.edu.cn/12570/list.htm', 'docu': 'column-news-con'},
+                {'link': 'https://syq.cumt.edu.cn/12571/list.htm', 'docu': 'column-news-con'},
+            ]
+        },
+{
+            'name': '',
+            'school_urls': [
+                {'link': '', 'docu': ''},
+                {'link': '', 'docu': ''},
+                {'link': '', 'docu': ''},
+                {'link': '', 'docu': ''},
+            ]
+        },
+]
+error_email = ['hcxy@cumt.edu.cn', 'rwxy@cumt.edu.cn']
+
+school_name = '中国矿业大学'
+c = TeacherEmail(error_email, school_name)
+for one_collage in collage_lists[-1:]:
+    c.open_chrome(one_collage)
+
+
+# TODO:国际学院，继续教育学院
+# 模板
+
+def muban():
+    a = \
+        {
+            'name': '',
+            'school_urls': [
+                {'link': '', 'docu': ''},
+                {'link': '', 'docu': ''},
+                {'link': '', 'docu': ''},
+                {'link': '', 'docu': ''},
+            ]
+        },
+    b = \
+        {
+            'name': '',
+            'school_urls': [
+                {'link': '', 'zth': '//*[@class=""]', 'nth': '', 'eth': ''},
+            ],
+        },
