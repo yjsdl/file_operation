8df9014c57eabaf35c5a73f39b42742d3b78b60c
workwindows <yj160521@163.com>
2023-11-08 15:43:56 +0800

    no

--- commen/save_file.py
+++ commen/save_file.py
@@ -78,4 +78,3 @@ if __name__ == '__main__':
     # c = SaveFile(path=r'F:\高校花名册\file_in\三江学院\花名册\学校别名表.xlsx')
     xlsx2csv(path=r'F:\蟹联网店铺信息\固城湖授权商家\固城湖店铺.xlsx')
     csv2xlsx(path=r'F:\高校花名册\file_in\上海交通大学\花名册\学校别名表_邮箱.csv')
-    'testtesttesttesttesttestest'
